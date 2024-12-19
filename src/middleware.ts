a91a0b40f25cfc107200e29c3ae0df821c47b2e0
Moritz Foglia <moritz.foglia@mni.thm.de>
2024-12-19 18:22:38 +0100

    fixed middleware?

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,17 @@ import { auth } from "~/auth";
 export const { auth: middleware } = NextAuth(authConfig);
 
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/auth/signin") {
-    const newUrl = new URL("/auth/signin", req.nextUrl.origin);
-    return Response.redirect(newUrl);
-  }
-});
+    const excludedPaths = ["/auth/signin", "/auth/signup"];
+    if (!req.auth && !excludedPaths.includes(req.nextUrl.pathname)) {
+      const newUrl = new URL("/auth/signin", req.nextUrl.origin);
+      return Response.redirect(newUrl);
+    }
+  });
+  
+
+  export const config = {
+    matcher: [
+      "/((?!api|_next/static|_next/image|favicon.ico|auth/signup).*)",
+    ],
+  };
+  
