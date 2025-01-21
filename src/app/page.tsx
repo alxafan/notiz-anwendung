9c01e5af4837114c43711a2c5f25740fa5612292
aafn <alexander.afanasjew@mni.thm.de>
2025-01-21 22:56:48 +0100

    Removed unused component, procedures and elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 import Link from "next/link";
 //figure out which import to use /auth /server/auth
-import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const session = await auth();
-
-  if (session?.user) {
-    void api.note.getLatest.prefetch();
-  }
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -33,9 +26,6 @@ export default async function Home() {
           </div>
           <div className="flex flex-col items-center gap-2">
             <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
-                {session && <span>Logged in as {session.user?.name}</span>}
-              </p>
               <Link
                 href="/api/auth/signout"
                 className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
