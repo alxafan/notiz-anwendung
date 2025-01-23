278025eb3ae90bc31c2171bf6be336d6b80a5c26
alxafn <afanasjewalexander@gmail.com>
2025-01-23 20:53:30 +0100

    Removed wrong auth api protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,6 @@ import { auth } from "~/auth";
 
 export const { auth: middleware } = NextAuth(authConfig);
 
-export default auth((req) => {
-  const excludedPaths = [
-    "/auth/signin",
-    "/auth/signup",
-    "/auth/getmail",
-    "/auth/reset-password/[token]",
-  ];
-  if (!req.auth && !excludedPaths.includes(req.nextUrl.pathname)) {
-    const newUrl = new URL("/auth/signin", req.nextUrl.origin);
-    return Response.redirect(newUrl);
-  }
-});
-
 export const config = {
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|auth/signup|auth/getmail|auth/reset-password/*).*)",
